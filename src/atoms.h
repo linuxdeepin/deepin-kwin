59c78d32ba83d6fcbe1f172b985de3d18589adb0
zccrs <zccrs@live.com>
2019-05-17 16:30:09 +0800

    feat: support _GTK_SHOW_WINDOW_MENU
    support _GTK_SHOW_WINDOW_MENU
    https://github.com/linuxdeepin/internal-discussion/issues/1320
    
    Log: support _GTK_SHOW_WINDOW_MENU
    Change-Id: I40c0e8fcc1a170eae374e5cf6b60b416b44916ff
    Signed-off-by: xupeidong <xupeidong@uniontech.com>

--- src/atoms.h
+++ src/atoms.h
@@ -62,6 +62,8 @@ public:
     Xcb::Atom kde_color_sheme;
     Xcb::Atom kde_skip_close_animation;
     Xcb::Atom kde_screen_edge_show;
+    Xcb::Atom gtk_frame_extents;
+    Xcb::Atom gtk_show_window_menu;
     Xcb::Atom kwin_dbus_service;
     Xcb::Atom utf8_string;
     Xcb::Atom text;
