fa37c93e20b20296c859309b70573b1da930cf27
Sian Cao <yinshuiboy@gmail.com>
2020-03-06 16:51:59 +0800

    fix: use bloom theme by default
    use bloom theme by default
    
    Log: use bloom theme by default
    Task: https://pms.uniontech.com/zentao/task-view-56975.html
    
    Change-Id: I88dced61727132cbd4f7cb890ba5c271ca98050b
    Signed-off-by: ut004146 <zhangqiyi@uniontech.com>

--- src/plugins/qpa/integration.cpp
+++ src/plugins/qpa/integration.cpp
@@ -35,6 +35,16 @@ namespace KWin
 
 namespace QPA
 {
+class DDEWaylandTheme: public QGenericUnixTheme {
+public:
+    QVariant themeHint(ThemeHint hint) const {
+        if (hint == QPlatformTheme::SystemIconThemeName) {
+            return "bloom";
+        }
+
+        return QGenericUnixTheme::themeHint(hint);
+    }
+};
 
 Integration::Integration()
     : QObject()
@@ -125,7 +135,7 @@ QPlatformFontDatabase *Integration::fontDatabase() const
 
 QPlatformTheme *Integration::createPlatformTheme(const QString &name) const
 {
-    return QGenericUnixTheme::createUnixTheme(name);
+    return new DDEWaylandTheme;
 }
 
 QStringList Integration::themeNames() const
