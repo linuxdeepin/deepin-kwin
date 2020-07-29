09b391398d3c928fc5c2d37ca15f3fb7a3d8fd66
zhanglianga <zhanglianga@uniontech.com>
2020-07-29 20:31:27 +0800

    fix: add the override role task:https://pms.uniontech.com/zentao/task-view-27563.html
    Change-Id: I06ebeea055f4f136e17ce890bf80e60ed8b3b44d

--- client.cpp
+++ client.cpp
@@ -1437,7 +1437,7 @@ void Client::takeFocus()
         if (workspace()->showingDesktop()) {
             // 最小化其它所有窗口
             for (Client *c : workspace()->clientList()) {
-                if (this == c || c->isDock() || c->isDesktop() || skipTaskbar() || (c->isUtility() && c->keepAbove()) || c->isStandAlone()) {
+                if (this == c || c->isDock() || c->isDesktop() || skipTaskbar() || (c->isUtility() && c->keepAbove())) {
                     continue;
                 }
 
