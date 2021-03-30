082aa3a68488030fcee1b030f8ca298c7d3aec03
zhangyu <zhangyud@uniontech.com>
2021-03-30 17:11:57 +0800

    fix: UT statistics mode
     UT statistics mode
    
    Log: UT statistics mode
    
    Change-Id: I1e6e28acfb920c03bf90899bed68496a1429624b

--- autotests/cmake-lcov-test.sh
+++ autotests/cmake-lcov-test.sh
@@ -3,35 +3,40 @@ workspace=$1
 
 cd $workspace
 
+mkdir build
+cd ./build
+
 export DISPLAY=:0.0
 
-dpkg-buildpackage -b -d -uc -us
+cmake ../
+make
+#dpkg-buildpackage -b -d -uc -us
 
 #make test
 
-project_path=$(cd `dirname $0`; pwd)
+#project_path=$(cd `dirname $0`; pwd)
 #获取工程名
-project_name="${project_path##*/}"
-echo $project_name
+#project_name="${project_path##*/}"
+#echo $project_name
 
 #获取打包生成文件夹路径
-pathname=$(find . -name obj*)
+#pathname=$(find . -name obj*)
 
-echo $pathname
+#echo $pathname
 
-cd $pathname
+#cd $pathname
 
 make test
 
-cd ./tests
+#cd ./tests
 
 mkdir -p coverage
 
 lcov -d ../ -c -o ./coverage/coverage.info
 
-lcov --remove ./coverage/coverage.info '*/tests/*' '*/autotests/*'  -o ./coverage/coverage.info
+lcov --remove ./coverage/coverage.info '*/tests/*' '*/autotests/*' '*/*_autogen/*' -o ./coverage/coverage.info
 
-mkdir ../report
-genhtml -o ../report ./coverage/coverage.info
+mkdir -p ../obj-x86_64-linux-gnu/report
+genhtml -o ../obj-x86_64-linux-gnu/report ./coverage/coverage.info
 
 exit 0
