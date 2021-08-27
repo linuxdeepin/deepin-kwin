94cff97aa08d5cefc03dc3a71ece7dfce0b1aa30
zhangyu <zhangyud@uniontech.com>
2021-08-27 17:41:04 +0800

    feat: add kwineffect interface to realize entering split screen from thumbnail
    add kwineffect interface to realize entering split screen from thumbnail
    
    Log: add kwineffect interface to realize entering split screen from thumbnail
    
    Change-Id: I947cf4b3af96374341e86014bb380e4bccb1b03f

--- effects/splitscreen/splitscreen.cpp
+++ effects/splitscreen/splitscreen.cpp
@@ -100,6 +100,7 @@ void SplitScreenEffect::paintWindow(EffectWindow *w, int mask, QRegion region, W
     WindowMotionManager& wmm = m_motionManagers[0];
     if (wmm.isManaging(w) || w->isDesktop()) {
         auto area = effects->clientArea(FullArea/*ScreenArea*/, m_screen, 0);
+
         WindowPaintData d = data;
         if (w->isDesktop()) {
             d.setBrightness(BRIGHTNESS);
@@ -476,7 +477,6 @@ void SplitScreenEffect::calculateWindowTransformationsClosest(EffectWindowList w
         EffectWindow *w = takenSlots[slot];
         if (!w) // some slots might be empty
             continue;
-
 
         // Work out where the slot is
         QRect target(
