22e99f4aa2e357ed19ecf79af58d248303cc2e59
gujinfeng <gujinfeng@uniontech.com>
2021-09-11 18:01:35 +0800

    Revert "fix: scale the cursorImage size when the screen is zoomed"
    This reverts commit 60b0d80fba0cd2cecf103e46b7a95ee9f9c5ab51.
    
    Reason for revert: <经确认不增加该代码的鼠标缩放逻辑与pgv和klua是一致的，基于pgv和klua都在4k屏下进行了对比实验，确认是效果一致的>
    
    Change-Id: Ic9d6b35b352d66e6a9ab510449f4ea2bed2abe26

--- plugins/platforms/drm/drm_output.cpp
+++ plugins/platforms/drm/drm_output.cpp
@@ -187,14 +187,7 @@ void DrmOutput::updateCursor()
     QRect cursorRect = QRect(QPoint(0, 0), cursorImage.size() / cursorImage.devicePixelRatio());
     p.setWorldTransform(logicalToNativeMatrix(cursorRect, 1, transformWayland()).toTransform());
 
-    if (qFuzzyCompare(scaleFactor(), qreal(1))) {
-        p.drawImage(QPoint(0, 0), cursorImage);
-    } else {
-        QImage cursorImageScaled = cursorImage.scaled(cursorImage.width() * scaleFactor()
-            , cursorImage.height() * scaleFactor(), Qt::KeepAspectRatio, Qt::SmoothTransformation);
-        p.drawImage(QPoint(0, 0), cursorImageScaled);
-    }
-
+    p.drawImage(QPoint(0, 0), cursorImage);
     p.end();
     if (workspace() && workspace()->isKwinDebug()) {
         qDebug() << "output" << uuid() << geometry() << globalPos()<<"draw cursorImag" << cursorImage.size() << c->size() << "scale" << cursorImage.devicePixelRatio();        
