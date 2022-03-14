7048c1e8c4cc68de3351544af39c63a81d441411
Lei Su <sulei@uniontech.com>
2022-03-14 20:26:17 +0800

    feat: Switch activate client when release left button
    This implement works on mouse. Other input device is not support.
    
    Log: In X11 switch activate client when release button
    
    Task: https://pms.uniontech.com/zentao/task-view-100762.html
    Change-Id: I15d246ec334e1b25417bddf5d4383bd8d688b7a3

--- workspace.h
+++ workspace.h
@@ -298,6 +298,9 @@ private:
 
 public:
 
+    void handleReleaseMouseCommond();
+    void setClientHandleMouseCommond(AbstractClient* c);
+    AbstractClient* clientHandleMouseCommond() const { return m_clientHandlingMouseCommond; };
     bool isDragingWithContent()const {return m_ClientDragingWithContent;}
 
     bool isOnCurrentHead();
@@ -773,6 +776,8 @@ private:
     bool m_kwinDebug = false;
     bool m_printKwinFps = false;
     bool m_dumpOutputBuffer = false;
+
+    AbstractClient* m_clientHandlingMouseCommond;
 
 private:
     friend bool performTransiencyCheck();
