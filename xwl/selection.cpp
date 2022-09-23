25df169b184c603394b80db51fa26daf643a5df9
Chaojiang Luo <luochaojiang@uniontech.com>
2022-09-23 17:22:17 +0800

    fix: Fix xwayland selection blocking
    1. skip same selection event
    2. skip quick event, < 2000ns
    3. skip ico image type
    
    Bug: https://pms.uniontech.com/bug-view-152469.html
    Log: Fix xwayland selection blocking
    
    Change-Id: Ica3460c8d1181a783c2730f5a0fa654368592121
    Signed-off-by: Chaojiang Luo <luochaojiang@uniontech.com>

--- xwl/selection.cpp
+++ xwl/selection.cpp
@@ -18,6 +18,8 @@
 
 #include <QTimer>
 
+#include <xwayland_logging.h>
+
 namespace KWin {
 namespace Xwl {
 
@@ -100,6 +102,18 @@ bool Selection::handleXfixesNotify(xcb_xfixes_selection_notify_event_t *event)
         m_timestamp = event->timestamp;
         return true;
     }
+
+    if (event == m_lastEvent) {
+        qCWarning(KWIN_XWL) << "xwl:skip same selection event " << event;
+        return true;
+    }
+    m_lastEvent = event;
+
+    if (event->timestamp - m_lastTimestamp < SELECTION_STRIDE_TIME) {
+        qCWarning(KWIN_XWL) << "xwl:skip quick selection event " << event;
+        return true;
+    }
+    m_lastTimestamp = event->timestamp;
 
     // Being here means some other X window has claimed the selection.
     doHandleXfixesNotify(event);
