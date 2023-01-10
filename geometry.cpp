d574ea3fcc99dd9b1f22c493aa0f535aed414bef
ding zhixuan <dingzhixuan@uniontech.com>
2023-01-10 19:49:57 +0800

    Revert "fix: splitoutline not hide when client send to this screen"
    This reverts commit fd71d74effd1651660c524cf53459f0f8c0e5ef6.

--- geometry.cpp
+++ geometry.cpp
@@ -3946,13 +3946,6 @@ void AbstractClient::sendToScreen(int newScreen)
             }
         }
     }
-
-    if (waylandServer()) {
-        if (m_moveResize.startScreen != newScreen) {
-            workspace()->updateSplitOutlineLayerShowHide();
-        }
-    }
-
     if (screen() == newScreen)   // Don't use isOnScreen(), that's true even when only partially
         return;
 
