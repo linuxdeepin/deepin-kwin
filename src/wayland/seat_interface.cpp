linuxdeepin/dwayland

feat: add fakeinput axis event for capture
为截图录屏软件增加了转发虚拟鼠标中键到鼠标位置所在app的功能（忽略最上层的截图录屏本身）

Log: 增加虚拟鼠标中键事件
Change-Id: I5e2c4dc9225069ac8be8df20e62a4ae7e5edfd94
Signed-off-by: ut004146 <zhangqiyi@uniontech.com>

src/server/seat_interface.cpp
@@ -676,6 +676,26 @@ void SeatInterface::notifyPointerAxis(Qt::Orientation orientation, qreal delta, 
     d->pointer->sendAxis(orientation, delta, discreteDelta, source);
 }
 
+void SeatInterface::notifyPointerAxisToClient(Qt::Orientation orientation, qint32 delta, SurfaceInterface * surface, QMatrix4x4 matrix)
+{
+    if (!d->pointer) {
+        return;
+    }
+    if (d->drag.mode == SeatInterfacePrivate::Drag::Mode::Pointer) {
+        // ignore
+        return;
+    }
+
+    const quint32 serial = d->display->nextSerial();
+    const QPointF pos = matrix.map(pointerPos());
+    SurfaceInterface * sur = d->pointer->focusedSurface();
+    if (sur != surface) {
+        d->pointer->setFocusedSurface(surface, pos, serial);
+    }
+    d->pointer->sendAxis(orientation, delta, 1, PointerAxisSource::Wheel);
+    usleep(10000);
+}
+
 void SeatInterface::notifyPointerButton(Qt::MouseButton button, PointerButtonState state)
 {
     const quint32 nativeButton = qtToWaylandButton(button);

linuxdeepin/deepin-kwin

src/wayland/seat_interface.cpp
@@ -674,6 +674,26 @@ void SeatInterface::notifyPointerAxis(Qt::Orientation orientation, qreal delta, 
     d->pointer->sendAxis(orientation, delta, deltaV120, source);
 }
 
+void SeatInterface::notifyPointerAxisToClient(Qt::Orientation orientation, qint32 delta, SurfaceInterface * surface, QMatrix4x4 matrix)
+{
+    if (!d->pointer) {
+        return;
+    }
+    if (d->drag.mode == SeatInterfacePrivate::Drag::Mode::Pointer) {
+        // ignore
+        return;
+    }
+
+    const quint32 serial = d->display->nextSerial();
+    const QPointF pos = matrix.map(pointerPos());
+    SurfaceInterface * sur = d->pointer->focusedSurface();
+    if (sur != surface) {
+        d->pointer->sendEnter(surface, pos, serial);
+    }
+    d->pointer->sendAxis(orientation, delta, 1, PointerAxisSource::Wheel);
+    usleep(10000);
+}
+
 void SeatInterface::notifyPointerButton(Qt::MouseButton button, PointerButtonState state)
 {
     const quint32 nativeButton = qtToWaylandButton(button);