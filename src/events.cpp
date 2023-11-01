b68188cd29e404fb46365515d178b2d62059a743
xinbo wang <wangxinbo@uniontech.com>
2023-11-01 10:40:57 +0800

    fix: modifier keys make x11-window type anything in wayland.
    modifier keys make x11-window type anything in wayland.
    
    Log: modifier keys make x11-window type anything in wayland.
    Bug: https://pms.uniontech.com/bug-view-225439.html
    Change-Id: I5c9ab9472ef22940d6fddd381021e513d41e6972

--- src/events.cpp
+++ src/events.cpp
@@ -1207,7 +1207,7 @@ void X11Window::focusOutEvent(xcb_focus_out_event_t *e)
     if (e->event != window()) {
         return; // only window gets focus
     }
-    if (e->mode == XCB_NOTIFY_MODE_GRAB) {
+    if (e->mode == XCB_NOTIFY_MODE_GRAB || e->mode == XCB_NOTIFY_MODE_UNGRAB) {
         return; // we don't care
     }
     if (isShade()) {
