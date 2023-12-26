776208bc727918acc34e792eccc8e5fbd850f580
luochaojiang <luochaojiang@uniontech.com>
2023-12-26 11:34:10 +0800

    Revert "fix: Fix screen flash for damage error"
    This reverts commit 5c47849f9779069d7548a6944ad0fc94d94863a8.
    
    Reason for revert: effects can not be updated.
    
    Change-Id: Ieeb1213e8a37bfb7f95b8093f3f32a65cafe501e

--- src/scene/workspacescene.cpp
+++ src/scene/workspacescene.cpp
@@ -297,14 +297,13 @@ void WorkspaceScene::preparePaintSimpleScreen()
             }
         }
 
+        effects->prePaintWindow(window->effectWindow(), data, m_expectedPresentTimestamp);
         m_paintContext.phase2Data.append(Phase2Data{
             .item = windowItem,
             .region = data.paint,
             .opaque = data.opaque,
             .mask = data.mask,
         });
-
-        effects->prePaintWindow(window->effectWindow(), data, m_expectedPresentTimestamp);
     }
 
     // Perform an occlusion cull pass, remove surface damage occluded by opaque windows.
