69ba6b0db869fc847bbe366b7d23d9d8103567e9
huangzhenyan <huangzhenyan@uniontech.com>
2024-02-23 14:43:05 +0800

    fix: Add Traditional Chinese and Uyghur menu and multitasking view translation entries
    add translation entries for traditional Chinese and Uyghur.
    
    Log: Add translation
    Bug: https://pms.uniontech.com/bug-view-235737.html
    Change-Id: Ie8605a58e65cc893aaf493e1c80d67c2cd775e09

---
+++ src/effects/multitaskview/translations/multitasking_ug.ts
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en">
+<context>
+    <name>KWin::MultitaskViewEffect</name>
+    <message>
+        <source>Drag upwards to remove</source>
+        <translation>يۇقىرىغا سۆرەش ئارقىلىق يۇيىۋەتمەك</translation>
+    </message>
+    <message>
+        <source>No windows</source>
+        <translation>ئېچىلغان كۆزنەك يوق</translation>
+    </message>
+</context>
+</TS>
